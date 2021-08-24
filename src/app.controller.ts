475c898a298ff9cd166570a7559e749b6fc06d11
Maxime Grimaud <maxime.grimaud@itga.fr>
2021-08-24 10:40:31 +0200

    Add auth with local strategy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
 import { AppService } from './app.service';
+
+
 
 @Controller()
 export class AppController {
@@ -8,5 +12,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -13,6 +15,8 @@ import { ItemsModule } from './items/items.module';
     }),
     ItemsModule,
     MongooseModule.forRoot('mongodb://localhost/nestgraphql'),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
